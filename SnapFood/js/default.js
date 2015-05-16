b399101242c2b4b6d8569119833d81fca402d4b7
Lisa W <miss.lisa19@hotmail.com>
2015-05-16 15:22:26 -0700

    new snapFood blank project

--- SnapFood/js/default.js
+++ SnapFood/js/default.js
@@ -9,7 +9,7 @@
 	app.onactivated = function (args) {
 		if (args.detail.kind === activation.ActivationKind.launch) {
 			if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
-				// TODO: This application has been newly launched. Initialize your application here.
+			    // TODO: This application has been newly launched. Initialize your application here.
 			} else {
 				// TODO: This application has been reactivated from suspension.
 				// Restore application state here.
@@ -17,6 +17,7 @@
 			args.setPromise(WinJS.UI.processAll());
 		}
 	};
+
 
 	app.oncheckpoint = function (args) {
 		// TODO: This application is about to be suspended. Save any state that needs to persist across suspensions here.
